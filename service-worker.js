7168343e67e7efd06330b1dacc4b62892b41380c
Ubden®🛡️ <44896964+ubden@users.noreply.github.com>
2024-10-01 15:23:32 +0300

    serviceworker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,32 @@
-self.addEventListener('push', function(event) {
+self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open('v1').then(cache => {
+        return cache.addAll([
+          '/index.php',
+          '/header.php',
+          '/footer.php',
+          '/icon.png'
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
+  // Push bildirimleri için ekleme
+  self.addEventListener('push', function(event) {
     const data = event.data.json();
-    const title = data.title;
     const options = {
-        body: data.body,
-        icon: 'icon.png',
-        badge: 'icon.png'
+      body: data.body,
+      icon: '/icon.png',
+      badge: '/icon.png',
     };
-
-    event.waitUntil(self.registration.showNotification(title, options));
-});
+    event.waitUntil(self.registration.showNotification(data.title, options));
+  });
+  
